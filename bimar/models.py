162cfae42d58c2f963c019c82b63b011e9de49cf
Your Name <you@example.com>
2024-09-06 01:24:25 -0700

    add-model

--- bimar/models.py
+++ bimar/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
+
+
+class Doctor(models.Model):
+    user = models.ForeignKey(User , on_delete=models.PROTECT)
+    date = models.DateField()
+
+class Khedmat(models.Model):
+    title = models.CharField(max_length=10)
+    price = models.DecimalField()
+    def __str__(self) -> str:
+        return self.title
+    
+class Nobat(models.Model):
+    bimar_name = models.ForeignKey(User , on_delete=models.PROTECT)
+    khedmat_type = models.ForeignKey(Khedmat , on_delete=models.PROTECT)
+    date = models.DateTimeField(auto_now_add=True)
+    doctor = models.ForeignKey(Doctor, on_delete=models.PROTECT)
+
+    def __str__(self) -> str:
+        return self.bimar_name
 # Create your models here.
--- bimar/views.py
+++ bimar/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.http.response import HttpResponse , JsonResponse
+from .models import Khedmat , Doctor ,Nobat
+from rest_framework.generics import ListAPIView, RetrieveAPIView , CreateAPIView,ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from .serialize import CopunSerializer , Transcreate , Factorserialize
+from rest_framework.permissions import IsAuthenticated , IsAdminUser ,AllowAny , 
+from rest_framework_simplejwt.views import TokenObtainPairView , token_refresh
 
+class login(TokenObtainPairView):
+    pass
+
+class refresh(token_refresh):
+    pass
+
+class NobatDetail(ListCreateAPIView):
+    queryset = Nobat.objects.all()
+    serializer_class = NobatSerializer
+    def get_permissions(self):
+        if self.action == 'POST':
+            self.permission_classes = [IsAdminUser]
+        elif self.action == 'GET':
+            self.permission_classes =[AllowAny]
+        return super().get_permissions()
 # Create your views here.
