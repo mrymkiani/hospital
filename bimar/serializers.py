d520a13b2422e6c772a240f7c3bbd05fc2dc7d97
Hamidreza <hamidreza.mugen@gmail.com>
2024-09-06 12:00:55 +0330

    serializer

---
+++ bimar/serializers.py
@@ -0,0 +1,18 @@
+from .models import Khedmat , Nobat
+from rest_framework import serializers
+
+
+
+class nobatserializer(serializers.ModelSerializer)
+    
+    class meta:
+        model = Nobat
+        field = '__all__'
+
+
+class khedmatserializer(serializers.ModelSerializer)
+    class meta:
+        model = Khedmat
+        field = '__all__'
+
+    
