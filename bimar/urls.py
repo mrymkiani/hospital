439326c5b73a2602e7f7773dae2134dcbc94e1df
Hamidreza <hamidreza.mugen@gmail.com>
2024-09-06 12:21:00 +0330

    urlpatterns

---
+++ bimar/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from bimar.views import (
+    NobatDetail,
+    EditNobat,
+    CreatKhedmat,
+    Payment
+)
+
+urlpatterns = [
+    path("Nobatdetail", NobatDetail.as_view()),
+    path('Editnobat/<int:pk', EditNobat.as_view()),
+    path('CreateKhedmat', CreatKhedmat.as_view()),
+    path('Payment', Payment.as_view()),
+
+]
